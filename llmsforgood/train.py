1fa5789c9587fe609551e51f24821ed6123ffde4
Michael Shtelma <michael.shtelma@databricks.com>
2024-06-05 16:10:21 +0200

    LoRA

--- llmsforgood/train.py
+++ llmsforgood/train.py
@@ -168,7 +168,7 @@ def run_training(script_args: ScriptArguments):
     ]
 
     generation_kwargs = {
-        "min_length": -1,
+        "min_length": -25,
         "top_k": 0.0,
         "top_p": 1.0,
         "do_sample": True,
