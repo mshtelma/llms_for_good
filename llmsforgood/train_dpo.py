58f959e5beb6f8420b1ef1dc6b5f57ec50879a8f
Michael Shtelma <michael.shtelma@databricks.com>
2024-06-07 16:23:30 +0200

    DPO support

--- llmsforgood/train_dpo.py
+++ llmsforgood/train_dpo.py
@@ -193,8 +193,8 @@ def run_training(script_args: ScriptArguments):
         gradient_checkpointing_kwargs=dict(
             use_reentrant=script_args.gradient_checkpointing_use_reentrant
         ),
-        max_length=script_args.max_length,
-        max_prompt_length=script_args.max_prompt_length,
+        # max_length=script_args.max_length,
+        # max_prompt_length=script_args.max_prompt_length,
         report_to=["mlflow"],
         num_train_epochs=script_args.num_train_epochs,
     )
@@ -250,13 +250,13 @@ def get_local_ip():
 
 if __name__ == "__main__":
     os.environ["MLFLOW_TRACKING_URI"] = "databricks"
-    os.environ["HF_HOME"] = "/tmp/hf"
-    os.environ["HF_DATASETS_CACHE"] = "/tmp/hf"
-    os.environ["TRANSFORMERS_CACHE"] = "/tmp/hf"
-    os.environ["NCCL_P2P_DISABLE"] = "1"
-    os.environ["NCCL_DEBUG"] = "INFO"
-    os.environ["NCCL_SOCKET_IFNAME"] = "eth0"
-    os.environ["HOST_IP"] = get_local_ip()
+    # os.environ["HF_HOME"] = "/tmp/hf"
+    # os.environ["HF_DATASETS_CACHE"] = "/tmp/hf"
+    # os.environ["TRANSFORMERS_CACHE"] = "/tmp/hf"
+    # os.environ["NCCL_P2P_DISABLE"] = "1"
+    # os.environ["NCCL_DEBUG"] = "INFO"
+    # os.environ["NCCL_SOCKET_IFNAME"] = "eth0"
+    # os.environ["HOST_IP"] = get_local_ip()
     parser = HfArgumentParser(ScriptArguments)
     script_args = parser.parse_args_into_dataclasses()[0]
     if script_args.train:
